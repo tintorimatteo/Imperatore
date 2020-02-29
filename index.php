380eb5ce20e03e555856712d1f8de079f9d4ecfa
Tintori <mattetinto@yahoo.it>
2020-02-29 17:41:18 +0100

    Added Poesy memo.

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ if($text=="forum"){
     $parameters = array('chat_id' => $chatId, "text" => "4rtista_Pr3ferito Minimal");
     $parameters["method"] = "sendMessage";
     echo json_encode($parameters);
-}else if($text=="jobmail"){
+}else if($text=="mail"){
     $parameters = array('chat_id' => $chatId, "text" => "HaoshokuYYYY");
     $parameters["method"] = "sendMessage";
     echo json_encode($parameters);
@@ -54,6 +54,43 @@ if($text=="forum"){
     echo json_encode($parameters);
 }else if($text=="biblioteca"){
     $parameters = array('chat_id' => $chatId, "text" => "cod.fs., 087v9xgm");
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+}else if($text=="metaphore"){
+    $content = "Nessuno,Età,Anthousa,Morte,VitaExtraordinaria,Destino,Gang-Klan-Setta,Casolimite,Scoperta,Disagio,Rimmel,Dio,Arte,Buio,"+
+        "Archimede-Marco-Odino-Zeus,Tecnologia,Guai,Lampada,Nuvola,Alba,Immortalità,Infinito,Emozioni,Roma,MiaCittà,Erba\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+     $content = "Mondo,Ieri,Pallone,Infanzia,Sigaretta,Invidia,Rabbia,Realtà\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+     $content = "Tutto,Resto(?),Vento,Governo,Nebbia,Cuore,Futuro,Sorriso,Penna,Maschera,Musica,Lei,Tu,Pace(?)\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+      $content = "Storia,Sabato,Noia,Fede,Ora,Secondo,Giorno,Ritorno,MezzoVolo,Rondine,Attimo,Giusy,Favola,Cuore(?),Sogno,BucoNero,Florenza"+
+          "Sincerità,VitaOrdinaria";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+      $content = "Coca,Saliva,PolvereDiStelle,Fuliggine,Suono,Birra,Ghiaccio,Acqua,Notte,Inferno,Canzone,Odio,Merdina,Mare,Oro,LibroAperto,"
+          +"HipHop,Buio\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+      $content = "Bianca,Paura,Cataclisma,Capolavoro,Amore\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+      $content = "Habibi,Lei,Flora,Europa,Qualcosa,Talento,Cosmo,Strada,Fine,Paradiso,Cielo,Natura,Energia(?),Spia,Faccia"
+          +"\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+      $content = "Amore,Roma,Minaccia,Lui\n";
+    $parameters = array('chat_id' => $chatId, "text" => $content);
     $parameters["method"] = "sendMessage";
     echo json_encode($parameters);
 }else{
